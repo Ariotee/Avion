66a44a462e1d1fe45df5ca7148c1d2034d8bb4db
Ariotee <mat_990011@mail.ru>
2024-09-26 04:25:18 +0700

    edit/add themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [],
+	content: ['./index.html', './src/**/*.{js,jsx}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			ibm: 'IBM Plex Mono',
+			satoshi: 'Satoshi',
+		},
+		extend: {
+			colors: {
+				'dark-hard': '#22202e',
+				purple: '#726e8d',
+			},
+		},
 	},
 	plugins: [],
 }
