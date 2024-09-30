02d71aadaaeadaaddb9f732b5439f74209d62e0e
Ariotee <mat_990011@mail.ru>
2024-10-01 02:19:08 +0700

    edit/new props

--- src/shared/ui/InputFields.jsx
+++ src/shared/ui/InputFields.jsx
@@ -9,18 +9,25 @@ export const SearchInput = () => {
 	)
 }
 
-export const JoinInput = ({ res = null, resForm = null }) => {
+export const JoinInput = ({
+	res = null,
+	resForm = null,
+	bg_color_btn = null,
+	text_color_btn = null,
+	bg_color_input = null,
+	text_color = null,
+}) => {
 	return (
 		<form className={`${resForm} flex justify-center items-center flex-nowrap`}>
 			<input
 				type='email'
 				name='email'
-				className={`${res} py-4 px-8 w-3/4 font-satoshi outline-dark-primary bg-light-gray`}
+				className={`${res} py-4 px-8 w-3/4 font-satoshi outline-dark-primary ${bg_color_input} ${text_color}`}
 				placeholder='your@email.com'
 			/>
 			<Button
-				bg_color={'text-white'}
-				text_color={'bg-dark-primary'}
+				bg_color={`${bg_color_btn}`}
+				text_color={`${text_color_btn}`}
 				type={'submit'}
 			>
 				Sign Up
