1ef6614e7fa65e295a5ded3243bd57d48a49aca9
Ariotee <mat_990011@mail.ru>
2024-09-30 22:20:53 +0700

    edit/create new input

--- src/shared/ui/InputFields.jsx
+++ src/shared/ui/InputFields.jsx
@@ -1,3 +1,4 @@
+import { Button } from './buttons'
 import { SearchIcon } from './Icons'
 
 export const SearchInput = () => {
@@ -5,5 +6,25 @@ export const SearchInput = () => {
 		<div className='cursor-pointer'>
 			<SearchIcon />
 		</div>
+	)
+}
+
+export const JoinInput = ({ res = null, resForm = null }) => {
+	return (
+		<form className={`${resForm} flex justify-center items-center flex-nowrap`}>
+			<input
+				type='email'
+				name='email'
+				className={`${res} py-4 px-8 w-3/4 font-satoshi outline-dark-primary bg-light-gray`}
+				placeholder='your@email.com'
+			/>
+			<Button
+				bg_color={'text-white'}
+				text_color={'bg-dark-primary'}
+				type={'submit'}
+			>
+				Sign Up
+			</Button>
+		</form>
 	)
 }
