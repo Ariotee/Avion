813a1dea27dc0ccbe11eebe1b61b5517fa25dfe2
Ariotee <mat_990011@mail.ru>
2024-09-30 22:20:36 +0700

    edit/type props

--- src/shared/ui/buttons.jsx
+++ src/shared/ui/buttons.jsx
@@ -2,11 +2,13 @@ export const Button = ({
 	bg_color = null,
 	text_color = null,
 	res = null,
+	type = null,
 	children,
 }) => {
 	return (
 		<button
 			className={`${bg_color} ${text_color} ${res} py-4 px-8 font-normal text-base leading-[150%] font-satoshi text-nowrap`}
+			type={`${type}`}
 		>
 			{children}
 		</button>
