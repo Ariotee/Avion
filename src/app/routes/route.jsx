5c3ccdcea6cd20da9a17ca02e1739f20f682ef7a
Ariotee <mat_990011@mail.ru>
2024-10-13 02:15:52 +0700

    edit/new router - auth

--- src/app/routes/route.jsx
+++ src/app/routes/route.jsx
@@ -1,7 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom'
-import { MainLayout } from '../../pages/layouts/MainLayout'
-import { ErrorPage } from '../../pages/Error/ErrorPage'
+
 import { Home } from '../../pages/Home/ui/Home'
+import { AuthPage } from '../../pages/Auth/AuthPage'
+import { ErrorPage } from '../../pages/Error/ErrorPage'
+import { AuthLayout } from '../../pages/layouts/AuthLayout'
+import { MainLayout } from '../../pages/layouts/MainLayout'
 
 export const router = createBrowserRouter([
 	{
@@ -15,6 +18,19 @@ export const router = createBrowserRouter([
 			},
 			{
 				path: '/collection',
+			},
+		],
+	},
+
+	{
+		path: '/auth',
+		element: <AuthLayout />,
+		errorElement: <ErrorPage />,
+
+		children: [
+			{
+				path: '/auth',
+				element: <AuthPage />,
 			},
 		],
 	},
